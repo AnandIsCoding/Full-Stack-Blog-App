0b0433ac06d864a00c0b6eb683e1427582f200c0
AnandIsCoding <anandkumarj669@gmail.com>
2024-12-01 16:28:26 +0530

    fixed checkauth

--- client/src/helpers/authHelper.js
+++ client/src/helpers/authHelper.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const authHelper = async () => {
   axios.defaults.withCredentials = true;
-  const res = await axios.get('http://localhost:3000/api/v1/checkauth', {
+  const res = await axios.get('https://genmyblog.onrender.com/api/v1/checkauth', {
     withCredentials: true,
   });
   const data = await res.data;
